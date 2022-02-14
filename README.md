03b10755e9d602c33e585ba04d77a4b327f356fa
Paul Anigbo <12402791+wizzypro@users.noreply.github.com>
2022-02-14 16:19:33 -0600

    implementation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spreadOperatorReact
-Created with CodeSandbox
+
+Practical implementation of spread operator in react
+check App.jsx for the code
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,26 +10,13 @@ function App() {
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setContact(prevValue => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value
-        };
-      }
+    setContact((prevValue) => {
+      console.log(prevValue);
+
+      return {
+        ...prevValue,
+        [name]: value
+      };
     });
   }
 
